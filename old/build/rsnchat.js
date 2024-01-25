7520fa882f7ff0f6aebb5608586f55f1d03ba6f5
AHQ Miness <ahqsecret@gmail.com>
2024-01-25 18:58:04 +0530

    Reduce code duplication

--- old/build/rsnchat.js
+++ old/build/rsnchat.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+const axios = require('axios');
 
 const GPT_ApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/gpt";
 const OpenChat_ApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/openchat";
@@ -11,323 +11,313 @@ const MixtralApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/mixtral";
 const ProdiaApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/prodia";
 const KandinskyApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/kandinsky";
 const AbsolutebeautyApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/absolutebeauty";
-const SdxlApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/sdxl";
+const SdxlApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/sdxl"; 
 const DalleApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/dalle";
 const IconApiUrl = "https://ai.rnilaweera.ovh/api/v1/user/icon";
 
-interface Result {
-  success: string;
-}
-
-interface TextResult extends Result {
-  message: string;
-}
-
-interface ProdiaResult extends Result {
-  imageUrl: string;
-  base64: string;
-}
-
-interface Image extends Result {
-  image: string;
-}
-
 class RsnChat {
-  headers = {
-    Authorization: "",
+  constructor(apiKey) {
+    if (!apiKey) {
+      throw new Error(`Please provide an Apikey`);
+    }
+    this.apiKey = apiKey;
   }
 
-  constructor(apikey: string) {
-    this.headers = {
-      Authorization: `Bearer ${apikey}`
-    };
-  }
-
-  /**
-   * Generate Text Completion via ChatGPT
-   * @param prompt 
-   * @returns 
-   */
-  async gpt(prompt: string) {
+  async gpt(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(GPT_ApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(GPT_ApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat GPT Error: ${error}`);
     }
   }
-
-  /**
- * Generate Text Completion via OpenChat
- * @param prompt 
- * @returns 
- */
-  async openchat(prompt: string) {
+  
+  async openchat(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(OpenChat_ApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(OpenChat_ApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat OpenChat Error: ${error}`);
     }
   }
-
-  /**
- * Generate Text Completion via Bard
- * @param prompt 
- * @returns 
- */
-  async bard(prompt: string) {
+  
+  async bard(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(Bard_ApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(Bard_ApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Bard Error: ${error}`);
     }
   }
-
-  /**
- * Generate Text Completion via Gemini
- * @param prompt 
- * @returns 
- */
-  async gemini(prompt: string) {
+  
+  async gemini(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(Gemini_ApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(Gemini_ApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Gemini Error: ${error}`);
     }
   }
-
-  /**
- * Generate Text Completion via LLaMa
- * @param prompt 
- * @returns 
- */
-  async llama(prompt: string) {
+  
+  async llama(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(LlamaApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(LlamaApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat LlaMa Error: ${error}`);
     }
   }
-
-  /**
- * Generate Text Completion via Mixtral
- * @param prompt 
- * @returns 
- */
-  async mixtral(prompt: string) {
+  
+  async mixtral(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(MixtralApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(MixtralApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Mixtral Error: ${error}`);
     }
   }
 
-  /**
- * Generate Text Completion via CodeLLaMa
- * @param prompt 
- * @returns 
- */
-  async codellama(prompt: string) {
+  async codellama(prompt) {
     try {
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(CodeLlamaApiUrl, payload, { headers: this.headers });
-      return response.data as TextResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(CodeLlamaApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat CodeLlaMa Error: ${error}`);
     }
   }
-
-  /**
- * Generate Image with Prodia
- * @param prompt 
- * @returns 
- */
-  async prodia(prompt: string, negative_prompt: string, model: string) {
+  
+  async prodia(prompt, negative_prompt, model) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       if (!negative_prompt) {
         return "No negative_prompt provided.";
       }
-
+      
       if (!model) {
         return "No model provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
         negative_prompt: negative_prompt,
         model: model,
       };
 
-      const response = await axios.post(ProdiaApiUrl, payload, { headers: this.headers });
-      return response.data as ProdiaResult;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(ProdiaApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Prodia Error: ${error}`);
     }
   }
-  /**
- * Generate Image with kandinsky
- * @param prompt 
- * @returns 
- */
-  async kandinsky(prompt: string, negative_prompt: string) {
+  
+  async kandinsky(prompt, negative_prompt) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       if (!negative_prompt) {
         return "No negative_prompt provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
         negative_prompt: negative_prompt,
       };
 
-      const response = await axios.post(KandinskyApiUrl, payload, { headers: this.headers });
-      return response.data as Image;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(KandinskyApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Kandinsky Error: ${error}`);
     }
   }
-  /**
- * Generate Image with AbsoluteBeauty
- * @param prompt 
- * @returns 
- */
-  async absolutebeauty(prompt: string, negative_prompt: string) {
+  
+  async absolutebeauty(prompt, negative_prompt) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       if (!negative_prompt) {
         return "No negative_prompt provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
         negative_prompt: negative_prompt,
       };
 
-      const response = await axios.post(AbsolutebeautyApiUrl, payload, { headers: this.headers });
-      return response.data as Image;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(AbsolutebeautyApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Absolutebeauty Error: ${error}`);
     }
   }
-
-  /**
-* Generate Image with SDXL
-* @param prompt 
-* @returns 
-*/
-  async sdxl(prompt: string, negative_prompt: string) {
+  
+  async sdxl(prompt, negative_prompt) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       if (!negative_prompt) {
         return "No negative_prompt provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
         negative_prompt: negative_prompt,
       };
 
-      const response = await axios.post(SdxlApiUrl, payload, { headers: this.headers });
-      return response.data as Image;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(SdxlApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Sdxl Error: ${error}`);
     }
   }
-  /**
-* Generate Image with DallE
-* @param prompt 
-* @returns 
-*/
-  async dalle(prompt: string) {
+  
+  async dalle(prompt) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(DalleApiUrl, payload, { headers: this.headers });
-      return response.data as Image;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(DalleApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Dalle Error: ${error}`);
     }
   }
-
-  /**
-* Generate Image with IconAPI
-* @param prompt 
-* @returns 
-*/
-  async icon(prompt: string) {
+  
+  async icon(prompt) {
     try {
       if (!prompt) {
         return "No prompt provided.";
       }
-
+      
       const payload = {
         prompt: prompt,
       };
 
-      const response = await axios.post(IconApiUrl, payload, { headers: this.headers });
-      return response.data as Image;
+      const authHeader = `Bearer ${this.apiKey}`;
+
+      const headers = {
+        Authorization: authHeader,
+      };
+
+      const response = await axios.post(IconApiUrl, payload, { headers });
+      return response.data;
     } catch (error) {
       throw new Error(`RsnChat Icon Error: ${error}`);
     }
   }
 }
 
-export {
-  RsnChat,
-  Result,
-  Image,
-  TextResult,
-  ProdiaResult
-};
+module.exports = RsnChat;
